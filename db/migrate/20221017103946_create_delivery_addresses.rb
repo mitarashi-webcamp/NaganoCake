99c074f1be2b5154b0bd393fc9cd596fcb5a21e3
P-toyo <111860935+P-toyo@users.noreply.github.com>
2022-10-18 02:37:43 +0000

    Input Column

--- db/migrate/20221017103946_create_delivery_addresses.rb
+++ db/migrate/20221017103946_create_delivery_addresses.rb
@@ -2,6 +2,11 @@ class CreateDeliveryAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :delivery_addresses do |t|
 
+      t.integer :customer_id
+      t.string  :name
+      t.string  :zip_code
+      t.text    :address
+
       t.timestamps
     end
   end
--- db/migrate/20221017104043_create_orders.rb
+++ db/migrate/20221017104043_create_orders.rb
@@ -1,6 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
+      t.integer :customer_id
+      t.string  :name
+      t.string  :zip_code
+      t.string  :address
+      t.integer :postage
+      t.integer :billing_amount
+      t.integer :total_payment
+      t.integer :status
 
       t.timestamps
     end
