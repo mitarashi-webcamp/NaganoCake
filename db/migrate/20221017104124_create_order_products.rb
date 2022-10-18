a4be4b1b8829f446f6b3ce9ee44d7a49a53773f4
EC2 Default User <ec2-user@ip-172-31-2-132.ap-northeast-1.compute.internal>
2022-10-18 02:37:43 +0000

    Input Column

--- db/migrate/20221017104124_create_order_products.rb
+++ db/migrate/20221017104124_create_order_products.rb
@@ -2,6 +2,12 @@ class CreateOrderProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :order_products do |t|
 
+      t.integer :order_id
+      t.integer :product_id
+      t.integer :price
+      t.integer :count
+      t.integer :making_status
+
       t.timestamps
     end
   end
