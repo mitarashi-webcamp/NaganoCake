5370182dc40a6362ac44bfb06670a708aacd9812
FujisawaAkifumi <112544215+fujisawa1234@users.noreply.github.com>
2022-10-19 10:56:00 +0000

    [Add]new_product

--- db/migrate/20221017104109_create_products.rb
+++ db/migrate/20221017104109_create_products.rb
@@ -6,7 +6,7 @@ class CreateProducts < ActiveRecord::Migration[6.1]
       t.string  :name
       t.text    :note
       t.integer :price
-      t.boolean :is_active
+      t.boolean :is_active, default: true
 
       t.timestamps
     end
