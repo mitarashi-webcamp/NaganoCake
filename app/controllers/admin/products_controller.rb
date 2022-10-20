dd1e65e700255131ed2c05bfdc0bcd7a581aaf5c
reizyte <112692614+reizyte@users.noreply.github.com>
2022-10-20 06:35:34 +0000

    admin_header,products_index

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -8,12 +8,16 @@ class Admin::ProductsController < ApplicationController
     @product.save
     redirect_to admin_product_path(@product)
   end
-  
-  
+
+  def index
+    @products = Product.all
+  end
+
+
   private
-  
+
   def product_params
     params.require(:product).permit(:genre_id,:name,:note,:price,:is_active,:image)
   end
-    
+
 end
