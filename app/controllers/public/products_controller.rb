4b9e10a6948c88126dac3292273ca62b0fabf835
ReinaYamamoto <112670391+reina9999@users.noreply.github.com>
2022-10-20 12:10:19 +0000

    Add

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,16 @@
 class Public::ProductsController < ApplicationController
+  before_action :authenticate_member!, only: [:show]
   def index
+    @products = Product.all
   end
 
   def show
+    @product=Product.find(params[:id])
+    @cart_product = CartProduct
+  end
+
+  private
+  def product_params
+    params.require(:products).permit(:genre_id,:name,:description,:image_id,:price)
   end
 end
