37f67a0d1a615968704e4511ccd4959ad11682d6
reizyte <112692614+reizyte@users.noreply.github.com>
2022-10-21 03:17:23 +0000

    admin/costomer_index,show,-add

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,31 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+   @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+       redirect_to admin_customers_path
+    else
+      redirect_to request.referer
+    end
+  end
+
+
+  private
+
+  def list_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :zip_code, :address, :phone_number, :email)
+  end
 end
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,2 +1,6 @@
 class Admin::HomesController < ApplicationController
+  before_action :authenticate_admin!
+  def top
+    @order_products = OrderProduct.all
+  end
 end
