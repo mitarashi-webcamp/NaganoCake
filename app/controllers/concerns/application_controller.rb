dd0dc5a8514f41e624746bf6bfab1ab2e7284a67
EC2 Default User <ec2-user@ip-172-31-2-132.ap-northeast-1.compute.internal>
2022-10-22 02:16:14 +0000

    Customer edit

--- app/controllers/concerns/application_controller.rb
+++ app/controllers/concerns/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  def autheniticate_customer
+    if @current_customer == nil
+      redirect_to(new_customer_session_path)
+    end
+  end
+
 end
