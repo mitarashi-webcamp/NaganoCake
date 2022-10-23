91a51fbd925587fa1fd6b51afc40bdadfb288fc6
P-toyo <111860935+P-toyo@users.noreply.github.com>
2022-10-23 08:29:37 +0000

    Update

--- app/controllers/public/delivery_addresses_controller.rb
+++ app/controllers/public/delivery_addresses_controller.rb
@@ -1,7 +1,24 @@
 class Public::DeliveryAddressesController < ApplicationController
   def index
+    @delivery_address = DeliveryAddress.new
+    @delivery_addresses = DeliveryAddress.where(customer_id: current_customer.id)
+  end
+
+  def create
+    delivery_address = DeliveryAddress.new(delivery_address_params)
+    delivery_address.customer_id = current_customer.id
+    delivery_address.save
+    redirect_to delivery_addresses_path
   end
 
   def edit
   end
+
+
+  private
+
+  def delivery_address_params
+    params.require(:delivery_address).permit(:zip_code, :address, :name)
+  end
+
 end
