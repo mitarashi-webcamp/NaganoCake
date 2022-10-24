35e9dd603584b4d708a9b04f29375c31ba436e16
ReinaYamamoto <112670391+reina9999@users.noreply.github.com>
2022-10-24 08:36:19 +0000

    add

--- app/controllers/public/carts_controller.rb
+++ app/controllers/public/carts_controller.rb
@@ -5,32 +5,39 @@ class Public::CartsController < ApplicationController
   end
 
   def create
-    @carts = current_customer.carts.new(cart_params)
+    @carts = Cart.new(cart_params)
+    @carts.customer_id = current_customer.id
+
     if current_customer.carts.find_by(product_id: params[:cart][:product_id]).present?
       current_customer.carts.find_by(product_id: params[:cart][:product_id])
        @carts.product_count += params[:cart][:product_count].to_i
        @carts.save!
        redirect_to carts_path
 
-    elsif @carts.save
-          @carts = current_customer.cart.all
-          render 'index'
+    elsif @carts.save!
+          redirect_to carts_path
 
     else  render 'index'
     end
   end
 
+  def update
+    @cart = Cart.find(params[:id])
+    @cart.update(product_count: params[:cart][:product_count].to_i)
+    redirect_to carts_path
+  end
+
   def destroy
     @cart_product = Cart.find(params[:id])
-     cart_product.destroy
+    @cart_product.destroy
     @cart_products = Cart.all
-     render 'index'
+     redirect_to carts_path
   end
 
   def all_destroy
     @cart_products = Cart.all
-    cart_products.destroy_all
-     render 'index'
+    @cart_products.destroy_all
+     redirect_to carts_path
   end
 
 
