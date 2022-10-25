7111699377e0fd738b69425ec1d1f2c46e7ed561
ReinaYamamoto <112670391+reina9999@users.noreply.github.com>
2022-10-25 08:53:31 +0000

    add

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,11 @@
 class Cart < ApplicationRecord
   belongs_to :product
   belongs_to :customer
+
+  
+  # def taxin_price
+  # end  
+  # def sum_price
+  #   product.taxin_price*product_count
+  # end
 end
