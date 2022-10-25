116c044d935b3b6cf642d44acdbd2b42636a0104
reizyte <112692614+reizyte@users.noreply.github.com>
2022-10-25 09:55:47 +0000

    enum-add

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -18,7 +18,7 @@ class Admin::CustomersController < ApplicationController
      if @customer.update(customer_params)
        redirect_to admin_customer_path(@customer)
      else
-        redirect_to request.referer
+         redirect_to request.referer
      end
 
 
