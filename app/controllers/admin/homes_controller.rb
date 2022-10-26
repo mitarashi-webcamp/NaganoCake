f7887b73e5a9f7c4b2a1ea53e3bd2982e1d0e904
reizyte <112692614+reizyte@users.noreply.github.com>
2022-10-26 06:58:30 +0000

    admin/top

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,12 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
+    @orders = Order.all
+    @order_new = Order.new
     @order_products = OrderProduct.all
+
+
+
+
   end
 end
